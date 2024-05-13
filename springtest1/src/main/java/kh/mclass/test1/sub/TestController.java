8850ce08c7565ae60f5a3a442d663c5a25aeae9f
JJOGGAN <withmelona1@gmail.com>
2024-05-13 14:10:22 +0900

    DB연동을 위한 pom설정
    jar 추가
    
    불필요한 주석 삭제

--- springtest1/src/main/java/kh/mclass/test1/sub/TestController.java
+++ springtest1/src/main/java/kh/mclass/test1/sub/TestController.java
@@ -18,26 +18,14 @@ public class TestController {
 	//private TestService service = new TestService;
 	@Autowired
 	private TestService testservice;
-	@Autowired
-	@Qualifier(value = "testDto2") //value를 넣어줘야함(그렇지 않으면 오류발생) 아니면 autowired를 사용하고 xml에서 지정한 id를 이름으로 지정해주면 된다
-	private TestDto testDto;
-	//spring에선 new로 객체를 선언하지 않고 에노테이션으로 호출
 	
-	//@Value("t1") //t1에 value지정가능 // 특정상황에서 특정 value값을 꺼내보고 싶을 때 사용되는 어노테이션
-	
-	//@RequestMapping(method = RequestMethod.GET, path="/test1")
 	@GetMapping("/test1")
 	public String method1(
 			@RequestParam(defaultValue = "default값이예요", required = false, name="a") String bbb, 
 			HttpServletRequest request,
 			HttpSession ss,
 			HttpServletResponse response) {
-		System.out.println("여기서 testDto 빈 확인하기");
-		System.out.println(testDto);
-		
 
-		
-		
 		
 		System.out.println(bbb);
 		System.out.println(request.getParameter("a"));
